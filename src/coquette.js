7f12c952155abda38930faa20f7993d98181990b
Eric Bréchemier <eric@bezen.org>
2013-05-14 19:01:59 +0200

    Update combined and minified scripts, now using within()

--- src/coquette.js
+++ src/coquette.js
@@ -1,30 +1,262 @@
-;(function(exports) {
-  var Coquette = function(game, canvasId, width, height, backgroundColor, autoFocus) {
-    coquette = this;
-    this.renderer = new Coquette.Renderer(canvasId, width, height, backgroundColor);
-    this.inputter = new Coquette.Inputter(canvasId, autoFocus);
-    this.updater = new Coquette.Updater();
-    this.entities = new Coquette.Entities();
-    this.runner = new Coquette.Runner();
-    this.collider = new Coquette.Collider();
+// https://github.com/eric-brechemier/within (License: CC0)
+// within is a factory of semi-private spaces
+// where properties and events can be shared.
+// Usage: within("your.domain", function(get, set, publish, subscribe){ ... });
 
+// from sub/nada/privately.js (CC0)
+function privately( func ) {
+  return func();
+}
+
+privately(function() {
+  var
+    dataSpaces = {},
+    eventSpaces = {},
+    has;
+
+  // from sub/nada/copy.js (CC0)
+  function copy( array ) {
+    return [].concat( array );
+  }
+
+  // from sub/nada/remove.js (CC0)
+  function remove( array, value ) {
+    var i;
+    for ( i = array.length; i >= 0; i-- ) {
+      if ( array[i] === value ){
+        array.splice( i, 1 );
+      }
+    }
+  }
+
+  // from sub/nada/forEach.js (CC0)
+  function forEach( array, callback ) {
+    var
+      isBreak = false,
+      i,
+      length = array.length;
+
+    for ( i = 0; i < length && !isBreak ; i++ ){
+      isBreak = callback( array[i], i ) === true;
+    }
+
+    return isBreak;
+  }
+
+  // from sub/nada/bind.js (CC0)
+  function bind( func, object ) {
+    return function() {
+      return func.apply( object, arguments );
+    };
+  }
+
+  /*
+    Define an alias for a prototype function
+    The alias allows to call the function with the context object
+    as first argument, followed with regular arguments of the function.
+    Example:
+    has = alias( Object.prototype.hasOwnProperty );
+    object.hasOwnProperty( name ) === has( object, name ); // true
+  */
+  function alias( func ) {
+    return bind( func.call, func );
+  }
+
+  has = alias( Object.prototype.hasOwnProperty );
+
+  /*
+    Create a semi-private space to share properties and events
+
+    Parameters:
+      name - string, name of the symbolic space:
+             a domain name and path that you control on the Web,
+             followed with the name of the module.
+             Example: "github.com/eric-brechemier/within/tests/module1"
+      callback - function( get, set, publish, subscribe ), function called
+                 immediately, in the context ('this') of an object,
+                 always the same in each call of within with the same name,
+                 and with four functions as arguments to share properties and
+                 events within this module (described separately below).
+
+    Returns:
+      any, the value returned by the callback function
+  */
+  function within( name, callback ) {
+    var
+      dataSpace,
+      eventSpace;
+
+    if ( !has( dataSpaces, name ) ) {
+      dataSpaces[name] = {};
+      eventSpaces[name] = {};
+    }
+
+    dataSpace = dataSpaces[name];
+    eventSpace = eventSpaces[name];
+
+    /*
+      Retrieve the value of a property previously set in this module
+
+      Parameter:
+        name - string, the name of a property of current module
+
+      Returns:
+        any, the value previously set to the property with given name,
+        or null initially before any value has been set
+    */
+    function get( name ) {
+      if ( !has( dataSpace, name ) ){
+        return null;
+      }
+      return dataSpace[name];
+    }
+
+    /*
+      Set the value of a property of the module
+
+      Parameters:
+        name - string, the name of a property in current module
+        value - any, the new value of the property
+
+      Note:
+      Calling this function is equivalent to setting the property directly
+      on the context object, and the function is only provided for symmetry
+      with get().
+    */
+    function set( name, value ) {
+      dataSpace[name] = value;
+    }
+
+    /*
+      Set the value of a property and fire listeners registered in this module
+      for the event of the same name
+
+      Parameters:
+        name - string, the name of an event and the associated property
+        value - any, the new value of the property, also provided to listeners
+
+      Notes:
+        1) Only listeners registered in this module are triggered: listeners
+        for an event of the same name in a module with a different name are
+        not fired.
+
+        2) The publication of the event will be interrupted by any listener
+        that returns the boolean value true. The following listeners, that
+        were registered later, will not be notified of the current value of
+        the event.
+    */
+    function publish( name, value ) {
+      var listeners;
+      set( name, value );
+      if ( !has( eventSpace, name ) ) {
+        return;
+      }
+      listeners = copy( eventSpace[name] );
+      forEach( listeners, function( listener ) {
+        return listener( value );
+      });
+    }
+
+    /*
+      Register a callback function for the event of given name
+
+      Parameters:
+        name - string, the name of an event and the related property
+        listener - function( value ), the callback triggered immediately
+                   with the current value of the property, if already set,
+                   and each time a new value is published for this property
+                   (not just set) unless a previous callback returns true
+                   which interrupts the publication of the current event.
+
+      Returns:
+        function(), the function to call to remove current listener, which
+        will no longer receive notifications for given event.
+
+      Notes:
+        1) In case the same listener is registered multiple times for the same
+        event, duplicate listeners are removed at the same time.
+        2) In case the same listener is registered to different events,
+        other subscriptions remain active and must be canceled separately.
+    */
+    function subscribe( name, listener ) {
+      var listeners;
+      if ( !has( eventSpace, name ) ) {
+        eventSpace[name] = [];
+      }
+      listeners = eventSpace[name];
+      listeners.push( listener );
+      if ( has( dataSpace, name ) ) {
+        listener( dataSpace[name] );
+      }
+      return function unsubscribe() {
+        remove( listeners, listener );
+      };
+    }
+
+    return callback.apply( dataSpace, [ get, set, publish, subscribe ] );
+  }
+
+  this.within = within;
+});
+
+var Coquette = within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
+  var Coquette = function(game, canvasId, width, height, backgroundColor, autoFocus) {
+    var
+      Renderer = get("Renderer"),
+      Inputter = get("Inputter"),
+      Updater = get("Updater"),
+      Entities = get("Entities"),
+      Runner = get("Runner"),
+      Collider = get("Collider");
+
+    set("game", game);
+    set("canvasId", canvasId);
+    set("width", width);
+    set("height", height);
+    set("backgroundColor", backgroundColor);
+    set("autoFocus", autoFocus);
+
+    set("coquette", this);
+    set("renderer", new Renderer(canvasId, width, height, backgroundColor));
+    set("inputter", new Inputter(canvasId, autoFocus));
+    set("updater", new Updater());
+    set("entities", new Entities());
+    set("runner", new Runner());
+    set("collider", new Collider());
+
+    // Public API
+    this.renderer = get("renderer");
+    this.inputter = get("inputter");
+    this.updater = get("updater");
+    this.entities = get("entities");
+    this.runner = get("runner");
+    this.collider = get("collider");
     this.updater.add(this.collider);
     this.updater.add(this.runner);
     this.updater.add(this.renderer);
     this.updater.add(game);
-    this.game = game;
+    this.game = get("game");
   };
 
-  var coquette;
   Coquette.get = function() {
-    return coquette;
+    return get("coquette");
   };
 
-  exports.Coquette = Coquette;
-})(this);
+  set("Coquette", Coquette);
 
-;(function(exports) {
+  // Public API in the browser
+  return Coquette;
+});
+
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   var Collider = function() {};
+
+  Collider.INITIAL = 0;
+  Collider.SUSTAINED = 1;
+
+  Collider.RECTANGLE = 0;
+  Collider.CIRCLE = 1;
+
 
   Collider.prototype = {
     collideRecords: [],
@@ -85,11 +317,11 @@
       }
     },
 
-    INITIAL: 0,
-    SUSTAINED: 1,
+    INITIAL: Collider.INITIAL,
+    SUSTAINED: Collider.SUSTAINED,
 
-    RECTANGLE: 0,
-    CIRCLE: 1
+    RECTANGLE: Collider.RECTANGLE,
+    CIRCLE: Collider.CIRCLE
   };
 
   var notifyEntityOfCollision = function(entity, other, type) {
@@ -115,17 +347,17 @@
     },
 
     isIntersecting: function(obj1, obj2) {
-      var obj1BoundingBox = obj1.boundingBox || Coquette.get().collider.RECTANGLE;
-      var obj2BoundingBox = obj2.boundingBox || Coquette.get().collider.RECTANGLE;
-      if (obj1BoundingBox === Coquette.get().collider.RECTANGLE &&
-          obj2BoundingBox === Coquette.get().collider.RECTANGLE) {
+      var obj1BoundingBox = obj1.boundingBox || Collider.RECTANGLE;
+      var obj2BoundingBox = obj2.boundingBox || Collider.RECTANGLE;
+      if (obj1BoundingBox === Collider.RECTANGLE &&
+          obj2BoundingBox === Collider.RECTANGLE) {
         return Maths.rectanglesIntersecting(obj1, obj2);
-      } else if (obj1BoundingBox === Coquette.get().collider.CIRCLE &&
-                 obj2BoundingBox === Coquette.get().collider.CIRCLE) {
+      } else if (obj1BoundingBox === Collider.CIRCLE &&
+                 obj2BoundingBox === Collider.CIRCLE) {
         return Maths.circlesIntersecting(obj1, obj2);
-      } else if (obj1BoundingBox === Coquette.get().collider.CIRCLE) {
+      } else if (obj1BoundingBox === Collider.CIRCLE) {
         return Maths.circleAndRectangleIntersecting(obj1, obj2);
-      } else if (obj1BoundingBox === Coquette.get().collider.RECTANGLE) {
+      } else if (obj1BoundingBox === Collider.RECTANGLE) {
         return Maths.circleAndRectangleIntersecting(obj2, obj1);
       } else {
         throw "Objects being collision tested have unsupported bounding box types."
@@ -240,11 +472,11 @@
     },
   };
 
-  exports.Collider = Collider;
-  exports.Collider.Maths = Maths;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
+  Collider.Maths = Maths;
+  set("Collider", Collider);
+});
 
- ;(function(exports) {
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   var Inputter = function(canvasId, autoFocus) {
     if (autoFocus === undefined) {
       autoFocus = true;
@@ -294,10 +526,11 @@
     DOWN_ARROW: 40,
     SPACE: 32
   };
-  exports.Inputter = Inputter;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
 
-;(function(exports) {
+  set("Inputter", Inputter);
+});
+
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   function Runner() {
     this.runs = [];
   };
@@ -322,10 +555,10 @@
     }
   };
 
-  exports.Runner = Runner;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
+  set("Runner", Runner);
+});
 
-;(function(exports) {
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   var interval = 16;
 
   function Updater() {
@@ -404,10 +637,10 @@
     }
   };
 
-  exports.Updater = Updater;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
+  set("Updater", Updater);
+});
 
-;(function(exports) {
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   var Renderer = function(canvasId, width, height, backgroundColor) {
     var canvas = document.getElementById(canvasId);
     canvas.style.outline = "none"; // stop browser outlining canvas when it has focus
@@ -436,15 +669,15 @@
     },
 
     onScreen: function(obj) {
-      return obj.pos.x > 0 && obj.pos.x < Coquette.get().renderer.width &&
-        obj.pos.y > 0 && obj.pos.y < Coquette.get().renderer.height;
+      return obj.pos.x > 0 && obj.pos.x < get("renderer").width &&
+        obj.pos.y > 0 && obj.pos.y < get("renderer").height;
     }
   };
 
-  exports.Renderer = Renderer;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
+  set("Renderer", Renderer);
+});
 
-;(function(exports) {
+within("coquette.maryrosecook.com", function(get, set, publish, subscribe) {
   function Entities() {
     this._entities = [];
   };
@@ -466,9 +699,9 @@
     },
 
     create: function(clazz, settings, callback) {
-      Coquette.get().runner.add(this, function(entities) {
-        var entity = new clazz(Coquette.get().game, settings || {});
-        Coquette.get().updater.add(entity);
+      get("runner").add(this, function(entities) {
+        var entity = new clazz(get("game"), settings || {});
+        get("updater").add(entity);
         entities._entities.push(entity);
         if (callback !== undefined) {
           callback(entity);
@@ -477,10 +710,10 @@
     },
 
     destroy: function(entity, callback) {
-      Coquette.get().runner.add(this, function(entities) {
-        Coquette.get().updater.remove(entity);
+      get("runner").add(this, function(entities) {
+        get("updater").remove(entity);
         entity._killed = true;
-        Coquette.get().updater.remove(entity);
+        get("updater").remove(entity);
         for(var i = 0; i < entities._entities.length; i++) {
           if(entities._entities[i] === entity) {
             entities._entities.splice(i, 1);
@@ -494,6 +727,6 @@
     }
   };
 
-  exports.Entities = Entities;
-})(typeof exports === 'undefined' ? this.Coquette : exports);
+  set("Entities", Entities);
+});
 
