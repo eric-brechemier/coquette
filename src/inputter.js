cd001e0bd64c13120ca9e53bab94038b160c9a46
Eric Bréchemier <eric@bezen.org>
2013-11-13 18:44:25 +0100

    Fix initialization of constants for codes of suppressed keys

--- src/inputter.js
+++ src/inputter.js
@@ -2,10 +2,10 @@ within("github.com/eric-brechemier/coquette", function(publish, subscribe) {
 
   var
     SPACE = 32,
-    LEFT_ARROW: 37,
-    UP_ARROW: 38,
-    RIGHT_ARROW: 39,
-    DOWN_ARROW: 40;
+    LEFT_ARROW = 37,
+    UP_ARROW = 38,
+    RIGHT_ARROW = 39,
+    DOWN_ARROW = 40;
 
   function preventScrolling(e) {
     var suppressedKeys = [
